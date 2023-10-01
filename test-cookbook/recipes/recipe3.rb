52538f948de2df8cd0a19f84703524501cfeb1db
root <root@ip-172-31-9-245.ap-south-1.compute.internal>
2023-10-01 05:57:09 +0000

    Attributes Added

--- test-cookbook/recipes/recipe3.rb
+++ test-cookbook/recipes/recipe3.rb
@@ -3,3 +3,10 @@
 # Recipe:: recipe3
 #
 # Copyright:: 2023, The Authors, All Rights Reserved.
+file '/mybasicinfo' do
+content "This is to get attributes
+HOSTNAME: #{node['hostname']}
+IPADDRESS: #{node['ipaddress']}
+MEMORY: #{node['memory']['total']}"
+action :create
+end
